3f8ee865c60057708319ea3b3867076288150144
Devsolution0216 <q3construction1@gmail.com>
2020-05-13 17:01:34 +0800

    Update setting.dart

--- lib/models/setting.dart
+++ lib/models/setting.dart
@@ -1,16 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 final databaseReference = Firestore.instance;
 
-void saveSetting() async {
-  await databaseReference.collection("settings")
-      .document("1")
+void saveSetting(String name) async {
+  await databaseReference.collection("users")
+      .document()
       .setData({
-        'name': 'Mastering Flutter',
+        'name': name,
       });
 
-  DocumentReference ref = await databaseReference.collection("settings")
-      .add({
-        'name': 'Flutter in Action',
-      });
-  print(ref.documentID);
+  // DocumentReference ref = await databaseReference.collection("settings")
+  //     .add({
+  //       'name': 'Flutter in Action',
+  //     });
+  // print(ref.documentID);
 }
