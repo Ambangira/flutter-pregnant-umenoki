e0659c3da347a09f5a9e7aabfb87e379258bf78d
Devsolution0216 <q3construction1@gmail.com>
2020-05-13 18:56:31 +0800

    Update setting.dart

--- lib/models/setting.dart
+++ lib/models/setting.dart
@@ -1,12 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:umenoki/models/auth.dart';
 
 final databaseReference = Firestore.instance;
 
-void saveSetting(String name) async {
+void saveSetting(List<String> settingData) async {
   await databaseReference.collection("users")
       .document()
       .setData({
-        'name': name,
+        'name': settingData[0],
+        'email': settingData[1],
       });
 
   // DocumentReference ref = await databaseReference.collection("settings")
@@ -15,3 +17,59 @@ void saveSetting(String name) async {
   //     });
   // print(ref.documentID);
 }
+
+
+void authValidateSubmit() async {
+  Auth auth = Auth();
+  String userId = "";
+  print(auth.currentUser());
+  auth.createUser("test@test.com", "testtest");
+  if (auth.currentUser() == null) {
+    try {
+      userId = await auth.signIn("test@test.com", "testtest");
+      // print(userId);
+      createRecord(userId);
+      return;
+    }
+    catch (e) {
+      // print(e);
+    }
+    if (userId.isEmpty) {
+      try {
+        userId = await auth.createUser("test@test.com", "testtest");
+      } catch (e) {
+        // print(e);
+      }
+    }
+    if (userId.isEmpty == false) {
+      try {
+        userId = await auth.signIn("test@test.com", "testtest");
+      } catch (e) {
+        // print(e);
+      }
+      createRecord(userId);
+    }
+  } else {
+    userId = await auth.currentUser();
+    // print(userId);
+  }
+}
+
+void createRecord(String userId) async {
+  try {
+    await databaseReference.collection("Users")
+        .document(userId)
+        .setData({
+      'name': 'Mastering Flutter',
+      'description': 'Programming Guide for Dart'
+    });
+  } catch (e) {
+    print(e);
+  }
+
+}
+
+
+
+
+
