b6709336bdde450d94bfe89be7b4cc42dbed615d
Devsolution <q3construction1@gmail.com>
2020-06-03 00:21:37 +0800

    Create custom_input.dart

---
+++ lib/src/widgets/custom_input.dart
@@ -0,0 +1,68 @@
+
+///
+/// Project name : Umenoki
+/// Description : The class is defined the custom input field for setting page.
+/// Author : Xiao
+/// Date : 2020-04-26
+///
+
+import 'package:flutter/material.dart';
+
+class InputField extends StatelessWidget {
+  final String label;
+  final String content;
+  final TextEditingController controller;
+  final bool obscureText;
+
+  InputField({
+    this.label, this.content = '', this.controller, this.obscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return Padding(
+          padding: const EdgeInsets.only(top:5, bottom: 5),
+          child: Row(
+            children: <Widget>[
+            Container(
+              child: Text(
+                "$label",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 17.0,
+                ),
+              ),
+            ),
+            Spacer(),
+            Container(
+              alignment: Alignment.center,
+              width: 200.0,
+              height: 40.0,
+              child: TextField(
+                controller: controller,
+                obscureText: obscureText,
+                style: TextStyle(
+                  fontSize: 17.0,
+                ),
+              
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: const BorderRadius.all(
+                      const Radius.circular(35.0),
+                    ),
+                  ),
+                  contentPadding: EdgeInsets.all(10.0),
+                  filled: false,
+                  hintText: "$content",
+                ),
+              ),
+            ),
+          ],
+          ),
+        );
+      },
+    );
+  }
+}
