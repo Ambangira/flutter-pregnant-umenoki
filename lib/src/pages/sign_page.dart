9badd8db3b39ff7a1a26f907e3b9a08ef72e5ac2
Devsolution <q3construction1@gmail.com>
2020-06-03 00:21:30 +0800

    Create sign_page.dart

---
+++ lib/src/pages/sign_page.dart
@@ -0,0 +1,27 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SignPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 60.0),
+            child: Container(
+              height: 80.0,
+              child: ImageIcon(
+                    AssetImage("assets/components/icn_map_health.png"),
+                    size: 50.0,
+                  ),
+            )
+          )
+        ]
+      ),
+    );
+  }
+}
