200119e3e45071e26f229b61f868609364cbce9d
Devsolution <q3construction1@gmail.com>
2020-06-03 00:21:24 +0800

    Create main_me_page.dart

---
+++ lib/src/pages/main_me_page.dart
@@ -0,0 +1,20 @@
+///
+/// Project name : Umenoki
+/// Description :
+/// Author : Xiao
+/// Date : 2020-04-26
+///
+
+import 'package:flutter/material.dart';
+
+/// This Widget is the main application widget.
+class MePage extends StatelessWidget {
+  static const String _title = 'Umenoki';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Me'),
+    );
+  }
+}
