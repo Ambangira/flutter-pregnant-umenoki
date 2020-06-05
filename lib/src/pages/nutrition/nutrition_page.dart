6f0d1cefe3c402a63ea2bf59d03e258619b57ca9
Devsolution <q3construction1@gmail.com>
2020-06-05 19:01:34 +0800

    Update nutrition_page.dart

--- lib/src/pages/nutrition/nutrition_page.dart
+++ lib/src/pages/nutrition/nutrition_page.dart
@@ -10,6 +10,8 @@ import 'package:flutter/material.dart';
 import 'package:umenoki/src/app_theme.dart';
 import 'package:umenoki/src/widgets/shape_border_widget.dart';
 
+import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
+
 class NutritionPage extends StatefulWidget {
 
   @override
@@ -21,9 +23,10 @@ class _NutritionPageState extends State<NutritionPage>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBarBuilder(context),
-      body: _bodyBuilder(context),
+      body: _bodyBuilder(context,)
     );
   }
+
 
   Widget _appBarBuilder(BuildContext context) {
     return AppBar(
@@ -45,10 +48,10 @@ class _NutritionPageState extends State<NutritionPage>{
         IconButton(
           icon: ImageIcon(
             AssetImage("assets/components/nutrition/icn_plus.png"),
-            size: 50.0,
+            size: AppTheme.iconSize,
             color: Colors.white,
           ),
-          iconSize: 50.0,
+          iconSize: AppTheme.iconSize,
           onPressed: (){
 
           },
@@ -58,6 +61,77 @@ class _NutritionPageState extends State<NutritionPage>{
   }
 
   Widget _bodyBuilder(BuildContext context) {
-    return Text('data');
+    return DefaultTabController(
+      length: 3,
+      child: Column(
+        children: <Widget>[
+          Container(
+            constraints: BoxConstraints.expand(height: 70.0),
+            child: TabBar(
+              indicatorSize: TabBarIndicatorSize.label,     // tab
+              indicator: BubbleTabIndicator(
+                indicatorHeight: 50.0,
+                indicatorColor: Color(0xFFFFE5E5),
+                tabBarIndicatorSize: TabBarIndicatorSize.tab,
+              ),
+              tabs: [
+                Tab(
+                  child: Container(
+                    decoration: BubbleTabIndicator(
+                      indicatorHeight: 48.0,
+                      indicatorColor: Color(0xFFFAC7C7),
+                      tabBarIndicatorSize: TabBarIndicatorSize.tab,
+                    ),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text("Recommended", style: TextStyle(color: Color(0xFFFE9E6F)),),
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Container(
+                    decoration: BubbleTabIndicator(
+                      indicatorHeight: 48.0,
+                      indicatorColor: Color(0xFFFAC7C7),
+                      tabBarIndicatorSize: TabBarIndicatorSize.tab,
+                    ),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text("Nutrients", style: TextStyle(color: Color(0xFFFE9E6F)),),
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50.0),
+                        border: Border.all(color: Color(0xFFFAC7C7), width: 1.0)),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text("Symptoms"),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ),
+          Expanded(
+            child: Container(
+              child: TabBarView(children: [
+                Container(
+                  child: Text('This is Recommended')
+                ),
+                Container(
+                  child: Text('This is Nutrients')
+                ),
+                Container(
+                  child: Text('This is Symptoms')
+                ),
+              ]),
+            )
+          )
+        ],
+      ),
+    );
   }
 }
